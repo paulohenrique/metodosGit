bb3c8aa389ef48fe940d02ac0ed5440aa9f6d6cb
Paulo Mota <Paulo Mota@192.168.25.5>
2015-09-01 08:53:22 -0300

    alteracoes

---
+++ src/main/java/br/com/starcode/testemaven01/ClasseDeTeste.java
@@ -0,0 +1,13 @@
+package br.com.starcode.testemaven01;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ClasseDeTeste {
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("Paulo Henrique"));
+
+    }
+
+}
---
+++ src/test/java/br/com/starcode/testemaven01/SuperUtilTest.java
@@ -0,0 +1,17 @@
+package br.com.starcode.testemaven01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SuperUtilTest {
+
+    @Test
+    public void escapeHTMLTest() {
+
+        String escapedHtml = SuperUtil.escapeHTML("<script>alert(1);</script>");
+        String expected = "&lt;script&gt;alert(1);&lt;/script&gt;"; 
+        Assert.assertEquals(expected, escapedHtml);
+
+    }
+
+}
