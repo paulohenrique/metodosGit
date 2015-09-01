c1d215e3ab4ea2fe82fb974d30af20ade9129b6d
Paulo Mota <Paulo Mota@10.2.19.236>
2015-09-01 17:43:35 -0300

    commit usando a branche teste

--- src/main/java/br/com/starcode/testemaven01/ClasseDeTeste.java
+++ src/main/java/br/com/starcode/testemaven01/ClasseDeTeste.java
@@ -6,7 +6,7 @@ public class ClasseDeTeste {
 
     public static void main(String[] args) {
 
-        System.out.println(StringUtils.capitalize("Paulo"));
+        System.out.println(StringUtils.capitalize("Paulo Mota"));
 
     }
 
